657d042192cd215936d301d8d8a4f196283181be
LEGIONDIABETICU\emild <emil.duschek@gmail.com>
2025-06-13 09:03:53 +0200

    initial commit + added horizontal movement, jump and ground check

---
+++ Assets/+++Workdata/Scripts/PlayerControls.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 6f;
+    [SerializeField] private float jumpForce = 8f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(inputX * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+
+}
